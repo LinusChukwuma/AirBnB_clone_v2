ff2c4dea8e5d655351b06bec4b001cfa4b49c93d
kouzin1 <samuelakinwunmi2@gmail.com>
2022-12-07 14:24:12 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,12 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     """Created Amenity class"""
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   viewonly=False)
+    place_amenities = relationship('Place', secondary='place_amenity')
